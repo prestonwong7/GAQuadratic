12e2ebc5a37ed07c4615c15dc04700f359baf3be
Preston Wong <prestonwong7@gmai>
2018-11-19 20:37:34 -0800

    Added some todo

--- Quadratic.cpp
+++ Quadratic.cpp
@@ -1,29 +1,43 @@
 #include "Quadratic.h"
 #include <iostream>
 #include <cmath>
+#include <stdlib.h>
 using namespace std;
 
 
-int getQuadraticFormula();
-int initialPopulation();
-int calculateFitness(int population[]);
-int selection();
+int getQuadraticFormula(int);
+int* calculateFitness(int population[]);
+int selection(int, int);
+int* crossover(int[]);
 int mutation();
 
+// fitness can be recipriocal of the number
+// Local search
+ // Population stays constant
+// Selection, roulette wheel
+// Aneeling, e^-delta d / t, d = distance from 0, t = temp (basically mutation)
+
+ 
+
 int main() {
-	//int fitness = getQuadraticFormula();
 	int initialPopulation[4] = {1, 2, 3, 4};
-	int fitness[4] = { 0,0,0,0 };
-	//initialPopulation();
-	calculateFitness(initialPopulation);
+	int fitness[4] = {1,2,3,4};
 	while (1) {
-		getQuadraticFormula();
-		calculateFitness();
-		selection();
+		calculateFitness(initialPopulation);
+		int mostFit = findMostFit(fitness);
+		int secondMostFit = findSecondMostFit(fitness);
+		selection(mostFit, secondMostFit);
+		crossover(initialPopulation);
 		mutation();
 	}
 	cin.get();
 	return 0;
+}
+
+int* calculateFitness(int population[])
+{
+	int fitness = getQuadraticFormula();
+	return population;
 }
 
 int getQuadraticFormula(int x) {
@@ -31,20 +45,37 @@ int getQuadraticFormula(int x) {
 	int b = 2;
 	int c = 1;
 	int formulaResult = a * pow(x, 2) + b * x + c;
-
 	return formulaResult;
 }
 
-int calculateFitness(int x)
+int selection(int mostFit, int secondMostFit)
 {
-	int fitness = getQuadraticFormula(x);
-
-	return fitness;
+	int child = (mostFit + secondMostFit) / 2;
+	return child;
 }
 
-int selection()
-{
-	return 0;
+int findMostFit(int fitness[]) {
+	int mostFit = fitness[0];
+	for (int i = 0; i < sizeof(fitness); i++) {
+		if (fitness[i] < mostFit) {
+			mostFit = fitness[i];
+		}
+	}
+	return mostFit;
+}
+
+int findSecondMostFit(int fitness[]) {
+	int mostFit = fitness[0];
+	for (int i = 0; i < sizeof(fitness); i++) {
+		if (fitness[i] < mostFit) {
+			mostFit = fitness[i];
+		}
+	}
+	return mostFit;
+}
+
+int* crossover(int initialPopulation[]) {
+
 }
 
 int mutation()
